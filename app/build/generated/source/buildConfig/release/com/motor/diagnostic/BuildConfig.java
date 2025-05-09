3abbe04bdecd93bcf3c98828c26b4a768f760862
EngrAlegre <qjisalegre@tip.edu.ph>
2025-05-09 09:49:35 +0800

    Initial commit of motorcycle diagnostic Android application

---
+++ app/build/generated/source/buildConfig/release/com/motor/diagnostic/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.motor.diagnostic;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.motor.diagnostic";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from build type: release
+  public static final boolean DEBUG_MODE = false;
+}
